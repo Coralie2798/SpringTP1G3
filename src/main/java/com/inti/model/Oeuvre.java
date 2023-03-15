907a401ab01e53d39a55fd215f94fa69a70ccf7b
libel <libel@LAPTOP-P1GR4IVV>
2023-03-15 14:32:55 +0100

    relation oeuvre concert

--- src/main/java/com/inti/model/Oeuvre.java
+++ src/main/java/com/inti/model/Oeuvre.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -21,5 +22,6 @@ public class Oeuvre {
 	private int Noeuvre;
 	private String nom;
 	private int duree;
+	
 	
 }
