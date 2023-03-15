d29124335e07f110ab0a971b98532e2559505817
Coralie2798 <coralie.bodiguel@gmail.com>
2023-03-15 14:25:05 +0100

    mise à jour classes oeuvre et soliste

--- src/main/java/com/inti/model/Oeuvre.java
+++ src/main/java/com/inti/model/Oeuvre.java
@@ -1,9 +1,14 @@
 package com.inti.model;
 
+import java.util.List;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -18,8 +23,20 @@ public class Oeuvre {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private int Noeuvre;
+	private int nOeuvre;
 	private String nom;
 	private int duree;
+	
+	@ManyToMany
+	@JoinTable(name="Soliste_Oeuvre", joinColumns = @JoinColumn(name="nOeuvre"), inverseJoinColumns = @JoinColumn(name="nSoliste"))
+	List<Soliste>listeSolistes;
+	
+	public Oeuvre(String nom, int duree) {
+		super();
+		this.nom = nom;
+		this.duree = duree;
+	}
+	
+	
 	
 }
--- src/main/java/com/inti/model/Soliste.java
+++ src/main/java/com/inti/model/Soliste.java
@@ -1,11 +1,17 @@
 package com.inti.model;
 
 import java.time.LocalDate;
+import java.util.List;
+
+import org.hibernate.annotations.ManyToAny;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -25,4 +31,18 @@ public class Soliste {
 	private String prenom;
 	private LocalDate dateNaissance;
 	private String nationnalite;
+	
+	@ManyToMany
+	@JoinTable(name="Soliste_Oeuvre", joinColumns = @JoinColumn(name="nSoliste"), inverseJoinColumns = @JoinColumn(name="nOeuvre"))
+	List<Oeuvre>listeOeuvre;
+	
+	public Soliste(String nom, String prenom, LocalDate dateNaissance, String nationnalite) {
+		super();
+		this.nom = nom;
+		this.prenom = prenom;
+		this.dateNaissance = dateNaissance;
+		this.nationnalite = nationnalite;
+	}
+	
+	
 }
