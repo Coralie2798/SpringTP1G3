ea33ce9c734bfdc39c6e9e303ba84349ab38b392
Niels <Niels@DESKTOP-48UCLNL>
2023-03-15 15:29:00 +0100

    Création du repository et du Controlleur

--- src/main/java/com/inti/controller/ConcertControlleur.java
+++ src/main/java/com/inti/controller/ConcertControlleur.java
@@ -1,8 +1,28 @@
-package com.inti.controller;
+package com.inti.controller;z
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.inti.model.Concert;
+import com.inti.repository.IConcertRepository;
 
 @Controller
 public class ConcertControlleur {
+	
+	IConcertRepository icrt;
+	
+	@GetMapping("formConcert")
+	public String formConcert()
+	{
+		return "formConcert";
+	}
 
+	@PostMapping("saveConcert")
+	public String saveConcert(@ModelAttribute("concert") Concert c)
+	{
+		icrt.save(c);
+		return "redirect/formConcert";
+	}
 }
