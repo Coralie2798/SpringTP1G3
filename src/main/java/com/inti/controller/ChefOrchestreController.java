a1bb8e311dded3cc4ad88ddaf06c2b3df308f6d6
theod <theod@DESKTOP-VBIVKTD>
2023-03-16 09:59:41 +0100

    Chef Orchestre terminé

--- src/main/java/com/inti/controller/ChefOrchestreController.java
+++ src/main/java/com/inti/controller/ChefOrchestreController.java
@@ -2,8 +2,10 @@ package com.inti.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.inti.model.ChefOrchestre;
@@ -25,7 +27,40 @@ public String formChefO()
 public String saveChefO(@ModelAttribute("ChefO") ChefOrchestre co)
 {
 	ichefr.save(co);
-	return "redirect/formChefO";
+	return "redirect:/formChefO";
+}
+
+@GetMapping("listeChefOrchestre")
+public String listeChefOrchestre(Model m)
+{
+	m.addAttribute("listeChefOrchestre", ichefr.findAll());
+	
+	return "listeChefOrchestre";
+}
+
+@GetMapping("deleteChefOrchestre/{id}")
+public String deleteChefOrchestre(@PathVariable("id") int id)
+{
+	ichefr.deleteById(id);
+	
+	return "redirect:/listeChefOrchestre";
+}
+
+@GetMapping("modifChefOrchestre/{id}")
+public String modifChefOrchestreVoiture(@PathVariable("id") int id, Model m)
+{
+	m.addAttribute("ChefOrchestre", ichefr.getReferenceById(id));
+	
+	return "modifChefOrchestre";
+}
+
+@PostMapping("updateChefOrchestre")
+public String updateChefOrchestre(@ModelAttribute("ChefOrchestre") ChefOrchestre co)
+{ 
+
+	ichefr.save(co);
+	
+	return "redirect:/listeChefOrchestre";
 }
 
 }
